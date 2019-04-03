dd9251dbc418ecbb44ea12fab4191c6edb6f952b
Florian Kraft <florian.kraft@kloeckner.com>
2019-04-03 15:21:44 +0200

    fresh project

---
+++ lib/postgres_sync_copy.ex
@@ -0,0 +1,18 @@
+defmodule PostgresSyncCopy do
+  @moduledoc """
+  Documentation for PostgresSyncCopy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PostgresSyncCopy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/postgres_sync_copy_test.exs
@@ -0,0 +1,8 @@
+defmodule PostgresSyncCopyTest do
+  use ExUnit.Case
+  doctest PostgresSyncCopy
+
+  test "greets the world" do
+    assert PostgresSyncCopy.hello() == :world
+  end
+end
